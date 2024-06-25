a93cbd37fcdeaf5ae902b8b73ba2fe5137dcc7dd
FerdinandoPH <perez.holguin@gmail.com>
2024-06-25 15:41:39 +0200

    mejor aen la eficiencia del savestate

--- chip8/emulator.py
+++ chip8/emulator.py
@@ -57,7 +57,7 @@ def main_loop(args):
                 elif keys_pressed[pygame.K_F5]:
                     print("Saving state...")
                     cpu.screen=None
-                    state=State(deepcopy(cpu),screen.save_state()[0],screen.save_state()[1],screen.save_state()[2],screen.save_state()[3])
+                    state=State(deepcopy(cpu),screen.save_state())
                     cpu.screen=screen
                     with open(args.rom.split("/")[-1]+"_state.dat","wb") as f:
                         pickle.dump(state,f)
--- chip8/state.py
+++ chip8/state.py
@@ -1,8 +1,8 @@
 class State():
-    def __init__(self,cpu,scale_factor,width,height,screen_array):
+    def __init__(self,cpu,screen_state):
         self.cpu=cpu
-        self.scale_factor=scale_factor
-        self.screen_array=screen_array
-        self.width=width
-        self.height=height
+        self.scale_factor=screen_state[0]
+        self.width=screen_state[1]
+        self.height=screen_state[2]
+        self.screen_array=screen_state[3]
         #self.screen_array=screen_array
